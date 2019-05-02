a08cbb4aa98004869e736e6801a3f20af627828a
kbohnenblust <kbohnenblust@raptus.com>
2019-05-02 14:22:30 +0200

    Changes with Jonas

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,7 +1,11 @@
 <?php snippet('header') ?>
 <div class="container-dark">
   <div class="container main" role="main">
-
+    <div class="row">  
+      <div class="col-md-12">
+        <h1><?= $page->title(); ?></h1>
+      </div>
+    </div>
     <div class="row">
 
       <div class="col-md-8 padding text-indent">
