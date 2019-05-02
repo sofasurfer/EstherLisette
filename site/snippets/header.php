f6e97e77689d7b811cd3f993c1fc05a3ef0dfe7c
sofasurfer <kilian@sofasurfer.ch>
2019-05-02 14:57:51 +0200

    Change version

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -13,7 +13,7 @@
   <?php echo seo('description'); ?>
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <?= css('assets/css/index.css?v=7.3') ?>
+  <?= css('assets/css/index.css?v=7.4') ?>
 
 
 </head>
