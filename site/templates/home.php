4dcb2cb356a6c75f8e0ba42f8f1bc0f43dd43f60
kib <kib@sofabook.local>
2019-05-20 22:39:17 +0200

    Ignore homer on card

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,7 +7,7 @@
       <?php foreach($page->imglist()->toStructure() as $project): ?>
 
           <div class="col-sm-6 col-md-4 showcase-thumb">
-              <a href="<?= $project->cta() ?>" class="showcase-link">
+              <a href="<?= $project->cta() ?>" class="showcase-link hover">
                 <?php if($image = $project->image()->toFile() ): $thumb = $image->crop(900, 900); ?>
                   <img src="<?= $image->url() ?>" alt="Thumbnail for <?= $project->title()->html() ?>" class="img-thumbnail" />
                 <?php endif ?>
